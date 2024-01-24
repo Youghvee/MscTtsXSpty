2916526dbc55ffb69345c807b9b211692ea1f5de
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-01-25 00:22:59 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
-#
-# This file is part of < https://github.com/kenkansaja/Musikku > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
-#
-# All rights reserved.
 
 import re
 import sys
