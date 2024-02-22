b223d443f1a0e87baa9a34a843f46d9d60ee1854
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-23 03:34:47 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -51,9 +51,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/TopMutualanGrup",
+    "https://github.com/Vygaee/Nature.git",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Musikku")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
@@ -102,7 +102,7 @@ YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://t.me/TopMutualanGrup")
+GITHUB_REPO = getenv("GITHUB_REPO", None)
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "f51dad0089144d93be494beb765dcc7a")
