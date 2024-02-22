a880fcbca46a8d99aa5cb37c0639a1b275660f83
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-23 04:19:09 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Vygaee/Nature.git",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
