2388ffc3881f0d2b3a24e8e5b4a5a56d23c70b0f
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-26 03:09:05 +0700

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+# Copyright (C) 2021-2022 by bee, < https://github.com/bee >.
 #
 # This file is part of < https://github.com/kenkansaja/Musikku > project,
 # and is released under the "GNU v3.0 License Agreement".
