25fce78763aff1fbc2f53ed4dda7ffaad8146efe
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-26 09:36:03 +0700

    Update __main__.py

--- AlexaMusic/__main__.py
+++ AlexaMusic/__main__.py
@@ -52,7 +52,7 @@ async def init():
     await userbot.start()
     await Alexa.start()
     try:
-        await Alexa.stream_call("https://telegra.ph/file/b60b80ccb06f7a48f68b5.mp4")
+        await Alexa.stream_call("https://mallucampaign.in/images/img_1708914936.jpg")
     except NoActiveGroupCall:
         LOGGER("AlexaMusic").error(
             "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
