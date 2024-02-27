1d80f7e3ea45b88de678ffd5c63340f9202b4b95
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-27 12:34:30 +0700

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -1,52 +1,15 @@
 # Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
 # Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
 
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from typing import Union
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from pyrogram.types import InlineKeyboardButton
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP, HERE_JOIN
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
 from AlexaMusic import app
 
 
-@Client.on_message(filters.incoming & filters.private, group=-1)
-async def here_join_channel(bot: Client, msg: Message):
-    if not HERE_JOIN:  # Not compulsory
-        return
-    try:
-        try:
-            await bot.get_chat_member(HERE_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if HERE_JOIN.isalpha():
-                link = "https://t.me/" + HERE_JOIN
-            else:
-                chat_info = await bot.get_chat(HERE_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply(
-                    f"Anda harus bergabung [Saluran ini]({link}) Untuk menggunakan saya. Jika sudah bergabung /start !",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup(
-                        [[InlineKeyboardButton("Join Channel", url=link)]]
-                    ),
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"ɪ'ᴍ ɴᴏᴛ ᴀᴅᴍɪɴ ɪɴ HERE_JOIN ᴄʜᴀᴛ : {HERE_JOIN} !")
-        
+       
 def start_pannel(_):
     buttons = [
         [
