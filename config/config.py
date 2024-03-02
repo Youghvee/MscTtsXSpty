5c4315c1dc0cd1a9602d9530cfc02af50a94fb12
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-02 15:45:29 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -51,7 +51,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vygaee/Manage-.git",
+    "https://github.com/Vooyage21/spotifyy.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
