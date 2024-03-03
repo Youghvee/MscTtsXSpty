6684b92d53fdd2b67cf4535ecb59c527a54cbe63
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-03 15:57:03 +0700

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -60,7 +60,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
-            InlineKeyboardButton(text="Support", url=f"https://t.me/SpotifyStreamMusic"),
+            InlineKeyboardButton(text="Support", url=f"https://t.me/MusicStreamMp3"),
         ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"
@@ -91,7 +91,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_3"],
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
-            InlineKeyboardButton(text="Support", url=f"https://t.me/SpotifyStreamMusic"),
+            InlineKeyboardButton(text="Support", url=f"https://t.me/MusicStreamMp3"),
         ],
     ]
     return buttons
