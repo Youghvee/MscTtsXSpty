3bd10666b5315394a03681f9e0dc0a3fd93ffd87
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-03 15:58:50 +0700

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -107,7 +107,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="Info", url=f"https://t.me/SpotifyStreamMusic/64"),
+            InlineKeyboardButton(text="Info", url=f"https://t.me/MusicStreamMp3"),
         ],
         [
             InlineKeyboardButton(
